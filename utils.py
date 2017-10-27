fe11060d78af19ca8d3ea5c37afbaf1e02a69991
bvasilis <bountris.vasilis@gmail.com>
2017-10-27 11:43:15 +0100

    Add batch normalization to functional, inception and seq_v2 models. This addition makes the recent change choice of 'raw' argument obsolete. Add a variety of activation functions. Add mean value in exploration script.

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def first_col_to_last(dataframe, columns):
     return df
 
 
-def create_dataset(path, deletedups=True, randomize=True, drop_digits=None, raw=False):
+def create_dataset(path, deletedups=False, randomize=True, drop_digits=6, raw=False):
     import pandas as pd
     import os
 
@@ -50,7 +50,7 @@ def create_dataset(path, deletedups=True, randomize=True, drop_digits=None, raw=
     dataset = pd.concat(frames, ignore_index=True)
 
     if raw:
-        dataset.iloc[:, :] = dataset.iloc[:, 1:].divide(4096)  # 4096 is the glove max output value reported by 5DT
+        dataset.iloc[:, 1:] = dataset.iloc[:, 1:].divide(4096)  # 4096 is the glove max output value reported by 5DT
 
     if deletedups:
         dataset = dataset.drop_duplicates().reset_index(drop=True)
@@ -107,19 +107,25 @@ def build_sequential_v1(input_dim, output_dim):
 
 def build_sequential_v2(input_dim, output_dim):
     from keras.models import Sequential
-    from keras.layers import Conv1D, Flatten, Dense
-    from keras.activations import relu, softmax
+    from keras.layers import Conv1D, Flatten, Dense, BatchNormalization, Dropout
+    from keras.activations import softmax, selu
 
+    ACTIVATION_FUNCTION = selu
     output_size = 32
     feature_map_size = 200
 
     model = Sequential()
-    model.add(Conv1D(output_size, 1, input_shape=(input_dim, 1), activation=relu))
-    model.add(Conv1D(output_size, 3, input_shape=(input_dim, 1), activation=relu))
-    model.add(Conv1D(output_size, 3, input_shape=(input_dim, 1), activation=relu))
+    model.add(BatchNormalization(input_shape=(input_dim, 1)))
+    model.add(Conv1D(output_size, 1, input_shape=(input_dim, 1), activation=ACTIVATION_FUNCTION))
+    model.add(Dropout(0.3))
+    model.add(Conv1D(output_size, 3, input_shape=(input_dim, 1), activation=ACTIVATION_FUNCTION))
+    model.add(Dropout(0.3))
+    model.add(Conv1D(output_size, 3, input_shape=(input_dim, 1), activation=ACTIVATION_FUNCTION))
+    model.add(Dropout(0.3))
 
     model.add(Flatten())
-    model.add(Dense(feature_map_size, activation=relu))
+    model.add(Dense(feature_map_size, activation=ACTIVATION_FUNCTION))
+    model.add(Dropout(0.3))
     model.add(Dense(output_dim, activation=softmax))
 
     return model
@@ -127,20 +133,22 @@ def build_sequential_v2(input_dim, output_dim):
 
 def build_functional(input_dim, output_dim):
     from keras.models import Model
-    from keras.layers import Conv1D, Flatten, Dropout, Dense, Input, concatenate
-    from keras.activations import relu, softmax
+    from keras.layers import Conv1D, Flatten, Dropout, Dense, Input, concatenate, BatchNormalization
+    from keras.activations import relu, softmax, selu
 
     DROPOUT_RATE = 0.5
-    ACTIVATION_FUNCTION = relu
-    output_size = 32
-    feature_map_size = 200
+    ACTIVATION_FUNCTION = selu
+    output_size = 16
+    feature_map_size = 100
 
     inputs = Input(shape=(input_dim, 1))
 
-    conv_1 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(inputs)
+    bn_input = BatchNormalization()(inputs)
+
+    conv_1 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(bn_input)
     conv_1 = Dropout(DROPOUT_RATE)(conv_1)
 
-    conv_2 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(inputs)
+    conv_2 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(bn_input)
     conv_2 = Conv1D(output_size, 3, activation=ACTIVATION_FUNCTION)(conv_2)
     conv_2 = Conv1D(output_size, 3, activation=ACTIVATION_FUNCTION)(conv_2)
     conv_2 = Dropout(DROPOUT_RATE)(conv_2)
@@ -158,38 +166,41 @@ def build_functional(input_dim, output_dim):
 
 def build_inception_layer(input_dim, output_dim):
     from keras.models import Model
-    from keras.layers import Conv1D, MaxPool1D, Flatten, Dropout, Dense, Input, concatenate
-    from keras.activations import relu, softmax
+    from keras.layers import Conv1D, MaxPool1D, Flatten, Dropout, Dense, Input, concatenate, BatchNormalization
+    from keras.activations import softmax, elu
 
     # TODO: Consider 'same', 'valid' options for padding
 
-    DROPOUT_RATE = 0.5
-    ACTIVATION_FUNCTION = relu
-    output_size = 32
+    DROPOUT_RATE = 0.3
+    ACTIVATION_FUNCTION = elu
+    output_size = 16
     pool_size = 3
-    feature_map_size = 200
+    feature_map_size = 50
 
     inputs = Input(shape=(input_dim, 1))
 
-    conv_1 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(inputs)
+    bn_input = BatchNormalization()(inputs)
+
+    conv_1 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(bn_input)
     conv_1 = Dropout(DROPOUT_RATE)(conv_1)
 
-    conv_2 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(inputs)
+    conv_2 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(bn_input)
     conv_2 = Conv1D(output_size, 3, activation=ACTIVATION_FUNCTION)(conv_2)
     conv_2 = Dropout(DROPOUT_RATE)(conv_2)
 
-    conv_3 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(inputs)
-    conv_3 = Conv1D(output_size, 5, activation=ACTIVATION_FUNCTION)(conv_3)
+    conv_3 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(bn_input)
+    conv_3 = Conv1D(output_size, 3, activation=ACTIVATION_FUNCTION)(conv_3)
+    conv_3 = Conv1D(output_size, 3, activation=ACTIVATION_FUNCTION)(conv_3)
     conv_3 = Dropout(DROPOUT_RATE)(conv_3)
 
-    pool_1 = MaxPool1D(pool_size, strides=1, padding='same')(inputs)
-    conv_4 = Conv1D(output_size, 1, activation=relu)(pool_1)
+    pool_1 = MaxPool1D(pool_size, strides=1, padding='same')(bn_input)
+    conv_4 = Conv1D(output_size, 1, activation=ACTIVATION_FUNCTION)(pool_1)
     conv_4 = Dropout(DROPOUT_RATE)(conv_4)
 
     output = concatenate([conv_1, conv_2, conv_3, conv_4], axis=1)
 
     flatten = Flatten()(output)
-    dense = Dense(feature_map_size, activation=relu)(flatten)
+    dense = Dense(feature_map_size, activation=ACTIVATION_FUNCTION)(flatten)
     dense = Dropout(DROPOUT_RATE)(dense)
     output = Dense(output_dim, activation=softmax)(dense)
 
